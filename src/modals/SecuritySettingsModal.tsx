dade87e70bdceee0a64a40d5cb0e22c00a7880dc
Ayodeji O <osasonaayodeji1@gmail.com>
2022-09-27 17:06:03 +0100

    Added security settings skeleton

--- src/modals/SecuritySettingsModal.tsx
+++ src/modals/SecuritySettingsModal.tsx
@@ -1,5 +1,5 @@
 import {AntDesign, Ionicons} from "@expo/vector-icons";
-import {Button, Divider, Heading, HStack, Icon, Text, VStack} from "native-base";
+import {Button, Divider, Heading, HStack, Icon, Switch, Text, VStack} from "native-base";
 import {Dimensions} from "react-native";
 import Modal from "../components/Modal";
 import ModalHeader from "../components/ModalHeader";
@@ -9,38 +9,43 @@ import {SecuritySettingsProps} from "../types/Settings";
 const {width} = Dimensions.get('window');
 
 export default function SecuritySettingsModal({visible, onClose}: SecuritySettingsProps) {
+
   return (
 	<Modal visible={visible} toggleVisibility={onClose}>
 	  <VStack space={4} pb={6}>
 		<ModalHeader>
 		  Security
 		</ModalHeader>
-		<PageButton
-		  title="Change password"
-		  description="Protect your account by changing your password"
-		  icon={{
-			as: AntDesign,
-			name: "key",
-		  }}
-		/>
+		<VStack space={1}>
+		  <PageButton
+			title="Change password"
+			description="Protect your account by changing your password"
+			icon={{
+			  as: AntDesign,
+			  name: "key",
+			}}
+		  />
 
-		<PageButton
-		  title="Change pin"
-		  description="Keep your files safe by setting a new pin"
-		  icon={{
-			as: Ionicons,
-			name: "keypad",
-		  }}
-		/>
+		  <PageButton
+			title="Change pin"
+			description="Keep your files safe by setting a new pin"
+			icon={{
+			  as: Ionicons,
+			  name: "keypad",
+			}}
+		  />
 
-		<PageButton
-		  title="Biometrics settings"
-		  description="Manage biometrics sign-in settings on this device"
-		  icon={{
-			as: Ionicons,
-			name: "ios-finger-print",
-		  }}
-		/>
+		  <HStack justifyContent="space-between" alignItems="center" px={3}>
+			<HStack alignItems="center">
+			  <Icon as={Ionicons} name="ios-finger-print" size={6} color="muted.400"/>
+			  <VStack space={0} ml={5}>
+				<Text color="muted.400" fontSize={17}>Biometrics</Text>
+				<Text width={width * 0.5} color="muted.600" fontSize={13}>Enable/disable biometrics unlock on this device</Text>
+			  </VStack>
+			</HStack>
+			<Switch size="sm" onTrackColor="primary.500" onThumbColor="muted.50" offTrackColor="muted.600" offThumbColor="muted.100"/>
+		  </HStack>
+		</VStack>
 	  </VStack>
 	</Modal>
   )
